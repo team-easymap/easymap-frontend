d7b03aed09a8eefbf78bd19532353ab795977711
snakechickensoup <141708304+snakechickensoup@users.noreply.github.com>
2024-07-26 18:52:19 +0900

    feat: 검색 기록 ui

--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -1,9 +1,11 @@
 import SearchComponent from '@/components/search/search-form/search';
+import SearchHistory from '@/components/search/search-history/search-history';
 
 const SearchPage = () => {
   return (
     <main className='h-full w-full'>
       <SearchComponent />
+      <SearchHistory />
     </main>
   );
 };
