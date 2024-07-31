1a07716aa28682ca25f6dc0ec1798467fe071514
snakechickensoup <141708304+snakechickensoup@users.noreply.github.com>
2024-07-31 20:35:26 +0900

    feat: react-leaflet 설치

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './globals.css';
+import './leaflet.css';
 
 import { RouterProvider } from 'react-router-dom';
 import router from './router/index.tsx';
