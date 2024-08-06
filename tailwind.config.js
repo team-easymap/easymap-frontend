58b3c171cdf8a5b247f8c79f9ce2f2749ab40054
snakechickensoup <141708304+snakechickensoup@users.noreply.github.com>
2024-08-06 19:58:13 +0900

    style: primary 색상 수정

--- tailwind.config.js
+++ tailwind.config.js
@@ -141,7 +141,8 @@ module.exports = {
       borderRadius: {
         lg: 'var(--radius)',
         md: 'calc(var(--radius) - 2px)',
-        sm: 'calc(var(--radius) - 4px)'
+        sm: 'calc(var(--radius) - 4px)',
+        xs: '3px'
       },
       keyframes: {
         'accordion-down': {
