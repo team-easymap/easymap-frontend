98f32c77bd2d6e75dd0310522667761f9053c59d
snakechickensoup <141708304+snakechickensoup@users.noreply.github.com>
2024-08-07 16:04:00 +0900

    feat: separator 컴포넌트 추가

--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -1,6 +1,7 @@
 import SearchComponent from '@/components/search/search-form/search';
 import SearchHistory from '@/components/search/search-history/search-history';
 import SearchRoutes from '@/components/search/search-routes/search-routes';
+import { Separator } from '@/components/ui/separator';
 import { useLocation } from 'react-router-dom';
 
 const SearchPage = () => {
@@ -13,6 +14,7 @@ const SearchPage = () => {
       ) : (
         <SearchRoutes />
       )}
+      <Separator className='h-2 bg-gray-1' />
       <SearchHistory />
     </main>
   );
