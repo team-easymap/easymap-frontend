7e3766ed128d2be88acb2082c42ed0072a91e992
snakechickensoup <141708304+snakechickensoup@users.noreply.github.com>
2024-08-07 20:58:27 +0900

    feat: useDebounce 커스텀훅 추가

---
+++ src/hooks/common/useDebounce.ts
@@ -0,0 +1,12 @@
+import { useEffect, useState } from 'react';
+
+export const useDebounce = <T>(value: T, delay: number = 400) => {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+
+  useEffect(() => {
+    const handler = setTimeout(() => setDebouncedValue(value), delay);
+    return () => clearTimeout(handler);
+  }, [value, delay]);
+
+  return debouncedValue;
+};
