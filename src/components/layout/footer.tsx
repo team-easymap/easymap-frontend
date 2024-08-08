777b90b632f1c2023bef49819d41bf6d9bdfa050
snakechickensoup <141708304+snakechickensoup@users.noreply.github.com>
2024-08-08 16:30:14 +0900

    feat: 페이지별 조건 레이아웃

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -2,10 +2,10 @@ import FooterButtonComponent from '../common/footerButton';
 
 const Footer = () => {
   return (
-    <footer className='absolute h-[10dvh] bottom-0 flex w-full justify-between border border-t-2 gap-6'>
-    <FooterButtonComponent icon="mapPin" title="주변" color="black"/>
-      <FooterButtonComponent icon="star" title="즐겨찾기" color="black"/>
-      <FooterButtonComponent icon="smile" title="마이" color="purple"/>  
+    <footer className='absolute bottom-0 flex h-[10dvh] w-full justify-between gap-6'>
+      <FooterButtonComponent icon='mapPin' title='주변' color='black' />
+      <FooterButtonComponent icon='star' title='즐겨찾기' color='black' />
+      <FooterButtonComponent icon='smile' title='마이' color='purple' />
     </footer>
   );
 };
