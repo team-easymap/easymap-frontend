e5b3faaaa62391b3bd5a206da3aac1fc38040a34
snakechickensoup <141708304+snakechickensoup@users.noreply.github.com>
2024-08-11 21:24:19 +0900

    feat: 장소 검색 결과 지도 표시

--- src/pages/search-poi-result.tsx
+++ src/pages/search-poi-result.tsx
@@ -1,5 +1,17 @@
+import SearchPOIResultHeader from '@/components/search-poi-result/result-header';
+import SearchPOIResultMap from '@/components/search-poi-result/result-map';
+import { mockPOI } from '@/mocks/poi';
+
 const SearchPOIResultPage = () => {
-  return <div>SearchPOIResultPage</div>;
+  const { lat, lng } = mockPOI;
+  return (
+    <>
+      <SearchPOIResultHeader />
+      <main className='relative flex flex-col'>
+        <SearchPOIResultMap center={[lat, lng]} />
+      </main>
+    </>
+  );
 };
 
 export default SearchPOIResultPage;
