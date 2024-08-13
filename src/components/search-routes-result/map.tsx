a3ddefb899741d17885e04c529af652e7b9ffb14
snakechickensoup <141708304+snakechickensoup@users.noreply.github.com>
2024-08-13 16:00:15 +0900

    feat: 경로 검색 결과 지도 표시

--- src/components/search-routes-result/map.tsx
+++ src/components/search-routes-result/map.tsx
@@ -1,5 +1,34 @@
+import { SearchRoutesResult } from '@/types/pois';
+import { MapContainer, Polyline, TileLayer } from 'react-leaflet';
+import { useLocation } from 'react-router-dom';
+
 const SearchRoutesResultMap = () => {
-  return <div>SearchRoutesResultMap</div>;
+  const key = import.meta.env.VITE_VWORLD_API_KEY;
+  const url = import.meta.env.VITE_VWORLD_API_URL;
+  const { state } = useLocation();
+
+  if (!state) {
+    return <>Error</>;
+  }
+
+  const route: SearchRoutesResult[number] = state.route;
+  const center = route.list[Math.floor(route.list.length / 2)];
+
+  return (
+    <MapContainer
+      center={center}
+      zoom={15}
+      scrollWheelZoom={true}
+      zoomControl={false}
+      style={{ height: '100dvh' }}
+      tap={false}>
+      <TileLayer
+        url={`${url}/req/wmts/1.0.0/${key}/Base/{z}/{y}/{x}.png`}
+        attribution="&copy; <a href='https://vworld.kr'>VWorld</a> contributors"
+      />
+      <Polyline positions={route.list} />
+    </MapContainer>
+  );
 };
 
 export default SearchRoutesResultMap;
