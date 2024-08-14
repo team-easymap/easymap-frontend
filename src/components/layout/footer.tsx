13a48767feefddabc512495317044494c5d9705e
snakechickensoup <141708304+snakechickensoup@users.noreply.github.com>
2024-08-14 10:41:39 +0900

    feat: 푸터 선택 버튼 색상 시정, navigate 추가

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -1,11 +1,28 @@
+import { useLocation, useNavigate } from 'react-router-dom';
 import FooterButtonComponent from '../common/footerButton';
 
 const Footer = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const selected = (path: string) => path === location.pathname;
+  const selectedColor = (path: string) => (selected(path) ? 'purple' : 'black');
   return (
     <footer className='absolute bottom-0 flex h-[10dvh] w-full justify-between gap-6'>
-      <FooterButtonComponent icon='mapPin' title='주변' color='black' />
-      <FooterButtonComponent icon='star' title='즐겨찾기' color='black' />
-      <FooterButtonComponent icon='smile' title='마이' color='purple' />
+      <FooterButtonComponent
+        icon='mapPin'
+        title='주변'
+        color={selectedColor('/pois')}
+        onClick={() => navigate('/pois')}
+        aria-selected={selected('/pois')}
+      />
+      {/* <FooterButtonComponent icon='star' title='즐겨찾기' color='black' /> */}
+      <FooterButtonComponent
+        icon='smile'
+        title='마이'
+        color={selectedColor('/my')}
+        onClick={() => navigate('/my')}
+        aria-selected={selected('/my')}
+      />
     </footer>
   );
 };
