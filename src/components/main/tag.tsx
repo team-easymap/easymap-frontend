1a7511e1a657c9f32fd6dd5c0f06d63f10cc1283
kmsjkh12 <kmsjkh12@naver.com>
2024-08-14 19:03:50 +0900

    lazy 로딩 적용

--- src/components/main/tag.tsx
+++ src/components/main/tag.tsx
@@ -1,6 +1,12 @@
 import { CommonButton } from '../common/commonButton';
 
-const tagList = ['Text', 'Text', 'Text', 'Text', 'Text', 'Text', 'Text'];
+const tagList = [
+  '식음료',
+  '생활시설',
+  '장애인용 화장실 ',
+  '휠체어 이동시설',
+  '장애물'
+];
 
 const TagComponent = () => {
   return (
@@ -14,6 +20,7 @@ const TagComponent = () => {
           onClick={() => {
             console.log('검색 값을 가지고 검색 페이지로 이동');
           }}>
+          {' '}
           {tag}
         </CommonButton>
       ))}
