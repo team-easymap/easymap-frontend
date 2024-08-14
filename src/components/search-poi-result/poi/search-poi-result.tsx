4301d44a9fd02e62686cdaa879bf980ba4ea0b49
snakechickensoup <141708304+snakechickensoup@users.noreply.github.com>
2024-08-14 11:28:08 +0900

    feat: 장소 검색 결과 버튼 수정

--- src/components/search-poi-result/poi/search-poi-result.tsx
+++ src/components/search-poi-result/poi/search-poi-result.tsx
@@ -1,9 +1,10 @@
-import { SearchResult, type POI } from '@/types/pois';
+import type { SearchResult, POI } from '@/types/pois';
 import SearchPOIResultButtons from './poi-buttons';
 import SearchPOIResultImages from './poi-images';
 import SearchPOIResultInfo from './poi-info';
 import SearchPOIResultTags from './poi-tags';
 import { Separator } from '@/components/ui/separator';
+import { useNavigate } from 'react-router-dom';
 
 type SearchPOIResultProps = {
   data: POI & SearchResult & { name: string };
@@ -11,21 +12,25 @@ type SearchPOIResultProps = {
 
 const SearchPOIResult = (props: SearchPOIResultProps) => {
   const { data } = props;
-  console.log(data);
+  const navigate = useNavigate();
   return (
     <div className='absolute bottom-0 z-[90000] w-full rounded-t-xl bg-white'>
       {data.poi_id ? (
         <>
-          <SearchPOIResultInfo {...data} />
-          <SearchPOIResultTags tags={data.tags_on_poi} />
-          <SearchPOIResultImages images={data.imgs_on_poi} />
-          <SearchPOIResultButtons isPOI={true} />
+          <div onClick={() => navigate(`/pois/${data.poi_id}`)}>
+            <SearchPOIResultInfo {...data} />
+            <SearchPOIResultTags tags={data.tags_on_poi} />
+            <SearchPOIResultImages images={data.imgs_on_poi} />
+          </div>
+          <SearchPOIResultButtons data={data} />
         </>
       ) : (
         <section>
-          <h2 className='px-4 pt-6 pb-8 text-18M'>{data.name}</h2>
+          <h2 className='px-4 pb-8 pt-6 text-18M'>
+            {data.name || data.address}
+          </h2>
           <Separator />
-          <SearchPOIResultButtons isPOI={false} />
+          <SearchPOIResultButtons data={data} />
         </section>
       )}
     </div>
