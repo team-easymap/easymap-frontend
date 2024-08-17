1fbd7d92f40964232d71d5e1d08df6a284df3df2
snakechickensoup <141708304+snakechickensoup@users.noreply.github.com>
2024-08-17 11:19:57 +0900

    feat: 장소 목록 탭 수정

--- src/components/main/poi-list/poi-list-tab.tsx
+++ src/components/main/poi-list/poi-list-tab.tsx
@@ -1,24 +1,26 @@
-const POIListTab = () => {
-  const categories = [
-    '전체',
-    '음식점',
-    '카페',
-    '장애인용 화장실',
-    '휠체어 충전소',
-    '공원'
-  ];
+import { CATEGORY_LIST } from '@/const/category';
+import { CategoryType } from './poi-list';
 
-  const active = '전체';
-  const activeColor = (category: (typeof categories)[number]) =>
-    category === active ? 'bg-primary' : 'bg-white';
+type POIListTabProps = {
+  handleTabClick: (category: CategoryType) => void;
+  activeTab: CategoryType;
+};
 
-  const activeText = (category: (typeof categories)[number]) =>
-    category === active ? 'text-primary' : 'text-gray-6';
+const POIListTab = (props: POIListTabProps) => {
+  const { handleTabClick, activeTab } = props;
+  const activeColor = (category: CategoryType) =>
+    category === activeTab ? 'bg-primary' : 'bg-white';
+
+  const activeText = (category: CategoryType) =>
+    category === activeTab ? 'text-primary' : 'text-gray-6';
 
   return (
     <ul className='flex w-full gap-4 px-4 overflow-x-auto text-14M'>
-      {categories.map((category) => (
-        <li key={category}>
+      {CATEGORY_LIST.map((category) => (
+        <li
+          key={category}
+          onClick={() => handleTabClick(category)}
+          className='cursor-pointer'>
           <div
             className={`whitespace-nowrap px-1 pb-1.5 pt-2.5 ${activeText(category)}`}>
             {category}
