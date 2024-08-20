bd6794413e123f00c562cb43e5297884c2b51fb2
snakechickensoup <141708304+snakechickensoup@users.noreply.github.com>
2024-08-20 23:00:34 +0900

    feat: 지도 클릭 이벤트 수정

--- src/components/common/select-location.tsx
+++ src/components/common/select-location.tsx
@@ -2,7 +2,7 @@ import { useMap } from 'react-leaflet';
 import IconComponent from '../ui/icon';
 import { CommonButton } from './commonButton';
 import { LeafletMouseEvent } from 'leaflet';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 
 type SelectLocationProps = {
@@ -14,11 +14,19 @@ const SelectLocation = (props: SelectLocationProps) => {
   const [isSelectMode, setIsSelectMode] = useState(!!state);
   const { handleMapClick } = props;
   const map = useMap();
-  map.on('click', (evt) => {
+
+  useEffect(() => {
     if (isSelectMode) {
-      handleMapClick(evt);
+      map.on('click', (evt) => {
+        const target = evt.originalEvent.target as HTMLElement;
+        if (target.classList.contains('leaflet-container')) {
+          handleMapClick(evt);
+        }
+      });
+    } else {
+      map.off('click');
     }
-  });
+  }, [handleMapClick, isSelectMode, map]);
 
   return (
     <CommonButton
@@ -26,7 +34,10 @@ const SelectLocation = (props: SelectLocationProps) => {
       color='white'
       radius='large'
       use='icon'
-      onClick={() => setIsSelectMode((prev) => !prev)}>
+      onClick={(e) => {
+        e.stopPropagation();
+        setIsSelectMode((prev) => !prev);
+      }}>
       <IconComponent
         name='mapPin'
         color={isSelectMode ? 'text-primary' : 'text-black'}
