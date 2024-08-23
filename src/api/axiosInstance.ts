3c148b6c0e48b8e098090c2260853316f32daa1a
snakechickensoup <141708304+snakechickensoup@users.noreply.github.com>
2024-08-23 17:11:53 +0900

    feat: axios instance 구현

---
+++ src/api/axiosInstance.ts
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+export const axiosInstance = axios.create({
+  baseURL: import.meta.env.VITE_BASE_URL,
+  withCredentials: true,
+  timeout: 6000
+});
+
+axiosInstance.interceptors.response.use(
+  (res) => res,
+  (err) => {
+    if (err.response && err.response.status === 403) {
+      const data = err.response.data;
+      if (data && data.code === 'AF') {
+        window.location.href = '/login';
+      }
+    }
+    return Promise.reject(err.message);
+  }
+);
